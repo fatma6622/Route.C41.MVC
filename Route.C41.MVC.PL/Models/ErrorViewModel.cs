fdc5e533be01dc6e94e832382f996f228bdf6676
fatma6622 <125834218+fatma6622@users.noreply.github.com>
2024-03-26 08:08:14 +0200

    Add project files.

---
+++ Route.C41.MVC.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Route.C41.MVC.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
