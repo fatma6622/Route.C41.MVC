00c27001f5b77bc086ce3e4c946acb17b247ec6b
fatma6622 <125834218+fatma6622@users.noreply.github.com>
2024-04-06 12:20:24 +0200

    Document Setting

---
+++ Route.C41.MVC.PL/Helpers/DocumentSettings.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+
+namespace Route.C41.MVC.PL.Helpers
+{
+    public static class DocumentSettings
+    {
+        public static string UploadFile(IFormFile file,string FolderName)
+        {
+            //string folderPath = $"C:\\Users\\Fatma\\Desktop\\ROUTE\\Backend\\tasks\\MVC\\new\\Route.C41.MVC\\Route.C41.MVC.PL\\wwwroot\\Files\\{FolderName}";
+            //string folderPath=$"{Directory.GetCurrentDirectory()}\\wwwroot\\Files\\{FolderName}";
+            string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Files", FolderName);
+            if(!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);
+            string fileName=$"{Guid.NewGuid()}{Path.GetExtension(file.Name)}";
+            string filePath=Path.Combine(folderPath, fileName);
+            using var fileStream = new FileStream(filePath, FileMode.Create);
+            file.CopyTo(fileStream);
+            return fileName;
+
+        }
+        public static void DeleteFile(string fileName, string FolderName)
+        {
+            string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Files", FolderName,fileName);
+            if(File.Exists(folderPath))
+                File.Delete(folderPath);
+
+        }
+    }
+}
